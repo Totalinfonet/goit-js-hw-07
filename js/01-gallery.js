ebec7b158c837a6228b7d81f95eb18fc29c6be0a
Totalinfonet <totalinfonet@gmail.com>
2022-12-26 15:19:18 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,65 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainerRef = document.querySelector(".gallery");
+
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+galleryContainerRef.insertAdjacentHTML("beforeend", galleryMarkup);
+
+galleryContainerRef.addEventListener("click", onGalleryContainerClick);
+
+function createGalleryMarkup(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+    })
+    .join("");
+}
+
+function onGalleryContainerClick(event) {
+  //   const clickedImageUrl = event.target.dataset.source;
+
+  //   event.preventDefault();
+
+  //   const eventTargetNodeName = event.target.nodeName;
+  //   console.log(eventTargetNodeName);
+  //   console.log(event);
+
+  //   if (eventTargetNodeName !== "IMG") {
+  //     return;
+  //   }
+  //   console.log(clickedImageUrl);
+  //   return clickedImageUrl;
+
+  event.preventDefault();
+  console.log(event);
+
+  const clickedEl = event.target;
+  console.log(clickedEl);
+
+  const isGalleryImageEl = clickedEl.classList.contains("gallery__image");
+  console.log(isGalleryImageEl);
+
+  if (!isGalleryImageEl) {
+    return;
+  }
+
+  const parentImageEl = clickedEl.closest(".gallery__link");
+  console.log(parentImageEl);
+
+  const clickedImageUrl = parentImageEl.href;
+  console.log(clickedImageUrl);
+  return clickedImageUrl;
+}
